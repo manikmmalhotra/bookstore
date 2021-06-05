91e79732155fa81526eb596fbf14f45ee72b2766
manikmmalhotra <manikmalhotra2113@gmail.com>
2021-06-05 20:12:51 +0530

    init

--- App.js
+++ App.js
@@ -1,34 +1,41 @@
 import React from 'react';
-import { createStackNavigator, DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  DefaultTheme,
+  NavigationContainer,
+} from '@react-navigation/native';
 
-import {BookDetail} from "./screens/";
-import tabs from "./navigation/tabs";
+import {BookDetail} from './screens/';
+import Tabs from './navigation/tabs';
 
 const theme = {
-    ...DefaultTheme,
-    colors:{
-      ...DefaultTheme.colors,
-      border: "transparent"
-    } 
-}
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: 'transparent',
+  },
+};
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer theme={theme}>
-    <Stack.Navigator
-          screenOptions={{
-            headerShown: false
-          }}
-          initialRouteName={'Home'}
-          >
-          {/* Tabs */}
-          <Stack.Screen name="Home" component={Tabs} />
-          
-          {/* Screens */}
-          <Stack.Screen name="BookDetail" component={BookDetail} options={{ headerShown: false }} />
-          </Stack.Navigator>
-          </NavigationContainer>
-  )
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={'Home'}>
+        {/* Tabs */}
+        <Stack.Screen name="Home" component={Tabs} />
+
+        {/* Screens */}
+        <Stack.Screen
+          name="BookDetail"
+          component={BookDetail}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
